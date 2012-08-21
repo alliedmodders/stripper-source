c09a516541c26eb1fbbb803e5808fa3692ee76b9
Dr!fter <Dr!fter@localhost>
2012-08-21 00:24:37 -0400

    Fix for loop to not error out on mac.

--- loader/stripper_loader.cpp
+++ loader/stripper_loader.cpp
@@ -173,7 +173,7 @@ EXPORT void *CreateInterface(const char *pName, int *pReturnCode)
 
 		/* Go backwards and get the first token */
 		size_t len = strlen(our_file);
-		for (size_t i = len - 1; i >= 0 && i < len; i--)
+		for (size_t i = len - 1; i-- > 0;)
 		{
 			if (IsPathSepChar(our_file[i]))
 			{
