67258b5ba412747c6835f911bbf16de48a2c4971
Eric Zhang <ericzhang456@disroot.org>
2026-09-11 15:54:38 +0800

    roll build script for pcre2

--- pcre.h
+++
@@ -1,8 +0,0 @@
-#ifndef _INCLUDE_STRIP_PCRE_H
-#define _INCLUDE_STRIP_PCRE_H
-
-#define PCRE2_CODE_UNIT_WIDTH 8
-#define PCRE2_STATIC
-#include "pcre/src/pcre2.h.generic"
-
-#endif /* _INCLUDE_STRIP_PCRE_H */
